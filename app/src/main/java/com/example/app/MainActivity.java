a12a898833925f13d853915e9b31d915fa1e44a7
yacine <yacine.brahim3100@gmail.com>
2024-11-23 04:04:00 -1100

    Create Project GL

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -36,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x;
-        int y;
+        
         }
     }
 
