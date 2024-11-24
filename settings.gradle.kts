8e639c32ec600284c52f213bb2748075f9906bea
benmaghnia amina <benmaghniaamina@benmaghnias-MacBook-Air.local>
2024-11-24 10:11:31 +0100

    Refresh

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "new_proj"
+include(":app")
